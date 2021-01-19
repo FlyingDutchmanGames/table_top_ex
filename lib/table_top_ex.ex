6b5cd1bf05fe6782c8f4f3adaf4020e93f75ba90
Grant Powell <email@grantjamespowell.com>
2021-01-19 01:51:15 -0500

    Add in projections.json

--- lib/table_top_ex.ex
+++ lib/table_top_ex.ex
@@ -1,17 +1,4 @@
 defmodule TableTopEx do
-  @moduledoc """
-  Documentation for `TableTopEx`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TableTopEx.hello()
-      :world
-
-  """
   def hello do
     :world
   end
