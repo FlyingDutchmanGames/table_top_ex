d8164cc22559e99205edfa0cb90135a636f60bda
Grant Powell <email@grantjamespowell.com>
2021-02-15 00:20:45 -0500

    Complete documenting tic tac toe

--- lib/table_top_ex/tic_tac_toe.ex
+++ lib/table_top_ex/tic_tac_toe.ex
@@ -11,9 +11,8 @@ defmodule TableTopEx.TicTacToe do
 
   defguardp on_board?(x) when x in [0, 1, 2]
 
-  @spec new() :: t()
   @doc ~S"""
-  Creates a new instance of TicTacToe
+  Creates a new instance of `TicTacToe`
 
       iex> game = %TicTacToe{} = TicTacToe.new()
       iex> TicTacToe.status(game)
@@ -25,14 +24,15 @@ defmodule TableTopEx.TicTacToe do
         {2, 0}, {2, 1}, {2, 2}
       ]
   """
+  @spec new() :: t()
   def new() do
     {:ok, ref} = NifBridge.tic_tac_toe_new()
     %__MODULE__{_ref: ref}
   end
 
-  @spec whose_turn(t()) :: player() | nil
   @doc ~S"""
-  Returns the player who's turn it is, games always start with `:P1`
+  Returns the player who's turn it is, games always start with `:P1`. This function
+  will return a value even if that player can't play because the game is won/drawn.
 
       iex> game = TicTacToe.new()
       iex> TicTacToe.whose_turn(game)
@@ -41,16 +41,16 @@ defmodule TableTopEx.TicTacToe do
       iex> TicTacToe.whose_turn(game)
       :P2
   """
+  @spec whose_turn(t()) :: player()
   def whose_turn(%__MODULE__{_ref: ref}) do
     {:ok, turn} = NifBridge.tic_tac_toe_whose_turn(ref)
     turn
   end
 
-  @spec board(t()) :: [[player() | nil]]
   @doc ~S"""
   Returns an Elixir reprensentation of the game board using lists.
-  The board is 0 indexed, with the first element of the first list being {0, 0}
-  and the last element of the last list being {2, 2}
+  The board is `0` indexed, with the first element of the first list being `{0, 0}`
+  and the last element of the last list being `{2, 2}`
 
       iex> game = TicTacToe.new()
       iex> TicTacToe.board(game)
@@ -68,12 +68,12 @@ defmodule TableTopEx.TicTacToe do
         [:P2, nil, nil]
       ]
   """
+  @spec board(t()) :: [[player() | nil]]
   def board(%__MODULE__{_ref: ref}) do
     {:ok, board} = NifBridge.tic_tac_toe_board(ref)
     board
   end
 
-  @spec history(t()) :: [move()]
   @doc ~S"""
   Returns a list of the moves that have been played in this game, in order.
 
@@ -85,24 +85,63 @@ defmodule TableTopEx.TicTacToe do
       iex> TicTacToe.history(game)
       [{:P1, {0, 0}}, {:P2, {0, 1}}]
   """
+  @spec history(t()) :: [move()]
   def history(%__MODULE__{_ref: ref}) do
     {:ok, history} = NifBridge.tic_tac_toe_history(ref)
     history
   end
 
+  @doc ~S"""
+  Returns all the currently open positions
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.available(game)
+      [
+        {0, 0}, {0, 1}, {0, 2},
+        {1, 0}, {1, 1}, {1, 2},
+        {2, 0}, {2, 1}, {2, 2}
+      ]
+      iex> pos = {0, 0}
+      iex> pos in TicTacToe.available(game)
+      true
+      iex> {:ok, game} = TicTacToe.apply_action(game, :P1, pos)
+      iex> pos in TicTacToe.available(game)
+      false
+  """
   @spec available(t()) :: [position()]
   def available(%__MODULE__{_ref: ref}) do
     {:ok, available} = NifBridge.tic_tac_toe_available(ref)
     available
   end
 
+  @doc ~S"""
+  Returns the status of the `TicTacToe` game, along with the winning positions if a player has won
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.status(game)
+      :in_progress
+  """
   @spec status(t()) :: {:win, player(), [position()]} | :in_progress | :draw
   def status(%__MODULE__{_ref: ref}) do
     NifBridge.tic_tac_toe_status(ref)
   end
 
-  @spec at_position(t(), position()) :: player() | nil
-  def at_position(game, {col_num, row_num}) when on_board?(col_num) and on_board?(row_num) do
+  @doc ~S"""
+  Returns the occupant of a position if there is one, distinguishing a position from being
+  empty and being off of the board. Positions are `0` indexed
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.at_position(game, {100, 100})
+      {:error, :position_outside_of_board}
+      iex> TicTacToe.at_position(game, {0, 0})
+      nil
+      iex> {:ok, game} = TicTacToe.apply_action(game, :P1, {0, 0})
+      iex> TicTacToe.at_position(game, {0, 0})
+      :P1
+  """
+  @spec at_position(t(), position()) :: player() | nil | {:error, :position_outside_of_board}
+  def at_position(game, {col_num, row_num} = _pos)
+      when on_board?(col_num) and on_board?(row_num) do
     board(game)
     |> Enum.at(col_num)
     |> Enum.at(row_num)
@@ -110,9 +149,51 @@ defmodule TableTopEx.TicTacToe do
 
   def at_position(_game, _position), do: {:error, :position_outside_of_board}
 
+  @doc ~S"""
+  Applies an action to an existing `TicTacToe` game. On success this function returns a new ref
+  and does _*NOT*_ mutate the original game. 
+
+  # Examples
+
+  Invalid players are invalid
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.apply_action(game, :something_random, {0, 0})
+      {:error, :invalid_player}
+
+  A player applying an action when it's not their turn is an error
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.whose_turn(game)
+      :P1
+      iex> TicTacToe.apply_action(game, :P2, {0, 0})
+      {:error, :other_player_turn}
+
+  You can't go in a taken space
+
+      iex> game = TicTacToe.new()
+      iex> {:ok, game} = TicTacToe.apply_action(game, :P1, {0, 0})
+      iex> TicTacToe.apply_action(game, :P2, {0, 0})
+      {:error, :space_is_taken}
+
+  You can't go outside the board
+
+      iex> game = TicTacToe.new()
+      iex> TicTacToe.apply_action(game, :P1, {100, 100})
+      {:error, :position_outside_of_board}
+
+  Other than the above, you can make a move and have it advance the game
+
+      iex> game = TicTacToe.new()
+      iex> {:ok, new_game} = TicTacToe.apply_action(game, :P1, {0, 0})
+      iex> TicTacToe.at_position(new_game, {0, 0})
+      :P1
+      iex> game == new_game
+      false
+  """
   @spec apply_action(t(), player(), position()) ::
           {:ok, t()} | {:error, :space_is_taken | :position_outside_of_board | :other_player_turn}
-  def apply_action(%__MODULE__{_ref: ref}, player, position) when player in [:P1, :P2] do
+  def apply_action(%__MODULE__{_ref: ref} = _game, player, position) when player in [:P1, :P2] do
     case NifBridge.tic_tac_toe_apply_action(ref, player, position) do
       {:ok, new_ref} when is_reference(new_ref) -> {:ok, %__MODULE__{_ref: new_ref}}
       {:error, err} -> {:error, err}
--- test/table_top_ex/tic_tac_toe_test.exs
+++ test/table_top_ex/tic_tac_toe_test.exs
@@ -4,43 +4,6 @@ defmodule TableTopEx.TicTacToeTest do
   alias TableTopEx.TicTacToe
   doctest TicTacToe
 
-  describe "apply_action/3" do
-    test "you can make a move" do
-      game = TicTacToe.new()
-      assert nil == TicTacToe.at_position(game, {0, 0})
-      assert {:ok, game} = TicTacToe.apply_action(game, :P1, {0, 0})
-      assert :P1 = TicTacToe.at_position(game, {0, 0})
-
-      assert [
-               [:P1, nil, nil],
-               [nil, nil, nil],
-               [nil, nil, nil]
-             ] == TicTacToe.board(game)
-    end
-
-    test "you can't go if it's not your turn" do
-      game = TicTacToe.new()
-      assert :P1 = TicTacToe.whose_turn(game)
-      assert {:error, :other_player_turn} = TicTacToe.apply_action(game, :P2, {0, 0})
-    end
-
-    test "you can't use a player that's not :P1 or :P2" do
-      game = TicTacToe.new()
-      assert {:error, :invalid_player} = TicTacToe.apply_action(game, :random_thingy, {0, 0})
-    end
-
-    test "you can't go in a taken space" do
-      game = TicTacToe.new()
-      assert {:ok, game} = TicTacToe.apply_action(game, :P1, {0, 0})
-      assert {:error, :space_is_taken} = TicTacToe.apply_action(game, :P2, {0, 0})
-    end
-
-    test "you can't go outside the board" do
-      game = TicTacToe.new()
-      assert {:error, :position_outside_of_board} = TicTacToe.apply_action(game, :P1, {100, 100})
-    end
-  end
-
   test "you can have a draw" do
     game = TicTacToe.new()
 
