6cf0bf69af70aa85922b8ec8651f8f4da34f8072
Grant Powell <email@grantjamespowell.com>
2021-02-15 23:41:44 -0500

    Add types around settings and dimensions

--- lib/table_top_ex/marooned/settings.ex
+++ lib/table_top_ex/marooned/settings.ex
@@ -1,6 +1,26 @@
 defmodule TableTopEx.Marooned.Settings do
   defmodule Dimensions do
-    @enforce_keys [:rows, :cols]
-    defstruct rows: 8, cols: 6
+    use TypedStruct
+
+    typedstruct enforce: true do
+      field(:rows, 0..255, default: 8)
+      field(:cols, 0..255, defualt: 6)
+    end
+
+    @spec(from_tuple({0..255, 0..255}), do: __MODULE__.t())
+    def from_tuple({rows, cols}), do: %__MODULE__{rows: rows, cols: cols}
+
+    @spec(to_tuple(__MODULE__.t()), do: {0..255, 0..255})
+    def to_tuple(%__MODULE__{rows: rows, cols: cols}), do: {rows, cols}
+  end
+
+  use TypedStruct
+
+  typedstruct do
+    field(:rows, 0..255, default: 3)
+    field(:cols, 0..255, default: 4)
+    field(:p1_starting, Marooned.position())
+    field(:p2_starting, Marooned.position())
+    field(:starting_removed_positions, [Marooned.position()], default: [])
   end
 end
